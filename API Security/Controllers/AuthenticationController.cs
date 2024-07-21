e0a9eb727ec94bebf6a84b316df801bfd0e1cf86
Abdualeem Hosny <abdualeem.hosny@nagwa.com>
2024-07-21 21:26:49 +0200

    update auth controller

--- API Security/Controllers/AuthenticationController.cs
+++ API Security/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
 ﻿using API_Security.Dto;
+using API_Security.helper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -18,59 +19,142 @@ namespace API_Security.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register([FromBody] )
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] UserRegistractionrDto userRegistractionrDto)
+        {
+            if (ModelState.IsValid)
+            {
+                //check user
+                var existing_user = await _userManager.FindByEmailAsync(userRegistractionrDto.Email);
 
-        //    [HttpPost]
+                if (existing_user != null)
+                {
+                    return BadRequest(new AuthResult()
+                    {
+                        Errors = new List<string>()
+                                    {
+                                        "Email already exist"
+                                    },
+                        Success = false
+                    });
+                }
+                //create new user
+                var new_user = new IdentityUser()
+                {
+                    Email = userRegistractionrDto.Email,
+                    UserName = userRegistractionrDto.Email
+                };
 
-        //    public async Task<IActionResult> Login([FromBody] LoginUserDto loginUserDto)
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            //check user
-        //            var existing_user= await _userManager.FindByEmailAsync(loginUserDto.Email);
+                var is_created=await _userManager.CreateAsync(new_user, userRegistractionrDto.Password);
 
-        //            if (existing_user == null) 
-        //            {
-        //                return BadRequest(new AuthResult()
-        //                {
-        //                    DataErrorsChangedEventArgs = new List<string>()
-        //                    {
-        //                        "Invalid payload"
-        //                    },
-        //                    Results = false
-        //                });
-        //            }
-        //            //check if the user is looked out
-        //            var is_locked_out = await _userManager.IsLockedOutAsync(existing_user);
+                if (is_created.Succeeded)
+                {
+                    //generate the token
 
-        //            if (is_locked_out) {
-        //                return Unauthorized(new AuthResult()
-        //                {
-        //                    DataErrorsChangedEventArgs = new List<string>()
-        //                    {
-        //                        "user is locked out please try again later"
-        //                    },
-        //                    Results = false
-        //                });
-        //            }
+                    // var token = new GenerateJwtToken(new_user);
 
-        //            var is_correct = await _userManager.CheckPasswordAsync(existing_user, loginUserDto.Password);
+                    return Ok(new AuthResult()
+                    {
 
-        //            if (!is_correct)
-        //            {
-        //                return BadRequest(new AuthResult()
-        //                {
-        //                    DataErrorsChangedEventArgs = new List<string>()
-        //                    {
-        //                        "Invalid credentials"
-        //                    },
-        //                    Results = false
-        //                });
-        //            }
+                        Success = true,
+                       // Token = token
+                    });
+                }
 
-        //            var jwt_token = GenerateJwtToken(existing_user);
-        //    }
-        //}
+                return BadRequest(new AuthResult()
+                {
+
+                    Success = true,
+                    Errors =  new List<string> { "server error"}
+                });
+
+            }
+            return BadRequest();
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login([FromBody] LoginUserDto loginUserDto)
+        {
+            if (ModelState.IsValid)
+            {
+                //check user
+                var existing_user = await _userManager.FindByEmailAsync(loginUserDto.Email);
+
+                if (existing_user == null)
+                {
+                    return BadRequest(new AuthResult()
+                    {
+                        Errors = new List<string>()
+                            {
+                                "Invalid payload"
+                            },
+                        Success = false
+                    });
+                }
+                //check if the user is looked out
+                var is_locked_out = await _userManager.IsLockedOutAsync(existing_user);
+
+                if (is_locked_out)
+                {
+                    return Unauthorized(new AuthResult()
+                    {
+                        Errors = new List<string>()
+                            {
+                                "user is locked out please try again later"
+                            },
+                        Success = false
+                    });
+                }
+
+                var is_correct = await _userManager.CheckPasswordAsync(existing_user, loginUserDto.Password);
+
+                if (!is_correct)
+                {
+                    // increace the locked count
+                    await _userManager.AccessFailedAsync(existing_user);
+
+                    // check if the user is lockedout
+
+                    var user_status = await _userManager.IsLockedOutAsync(existing_user);
+
+                    if (user_status)
+                    {
+                        return Unauthorized(new AuthResult()
+                        {
+                            Errors = new List<string>()
+                            {
+                                "user is locked out please try again later"
+                            },
+                            Success = false
+                        });
+                    }
+               
+                        return BadRequest(new AuthResult()
+                        {
+                            Errors = new List<string>()
+                            {
+                                "Invalid credentials"
+                            },
+                            Success = false
+                        });
+                    
+
+
+                }
+
+                await _userManager.ResetAccessFailedCountAsync(existing_user);
+                var jwt_token = GenerateJwtToken(existing_user);
+
+                return Ok(new AuthResult()
+                {
+                   Token= jwt_token,
+                    Success = true
+                });
+
+            }
+        }
 
     }
 }
---
+++ API Security/helper/AuthResult.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Security.helper
+{
+    public class AuthResult
+    {
+        public List<string> Errors { get; set; }
+        public bool Success { get; set; }
+        public string Token { get; set; }
+    }
+}
