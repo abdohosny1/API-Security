0c06265e35daba75e2c9e75b974e80b131962958
abdohosny1 <make.y90@yahoo.com>
2024-07-23 20:27:47 +0300

    update_migration

---
+++ API Security/Migrations/20240723172624_add_role.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API_Security.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_role : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "8afce009-d6d9-42d2-8879-5f2448df9ca4", null, "Admin", "ADMIN" },
+                    { "ebffca6e-070c-49d9-8363-5f944c1173d7", null, "Team", "TEAM" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8afce009-d6d9-42d2-8879-5f2448df9ca4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ebffca6e-070c-49d9-8363-5f944c1173d7");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+        }
+    }
+}
